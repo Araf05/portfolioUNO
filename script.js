7f23ada6644c15faedc5d632bc43af3798757989
Araceli Fernandez <aracelifernandez.3006@live.com.ar>
2023-03-15 23:14:28 -0300

    Di estilos al index, a los section principal y acerca. Agregue hoja memo.html y su hoja de estilos y js

---
+++ script.js
@@ -0,0 +1,14 @@
+const emojis = ['', '', '', ]
+
+/* constatntes variables que no se modifican*/
+
+const selectors = {
+    boardContainer: document.querySelector('.board-container'),
+    board: document.querySelector('.board'),
+    movimientos: querySelector('.movimientos'),
+    tiempo: document.querySelector('.tiempo'),
+    start: document.querySelector('#start'),
+    restart: document.querySelector('#restart'),
+    ganaste: document.querySelector('.ganaste'),
+}
+
