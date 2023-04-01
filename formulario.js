ff62d788ff07af435944fe352f44f7ad53ce870b
Araceli Fernandez <aracelifernandez.3006@live.com.ar>
2023-03-31 22:43:38 -0300

    Menu flex, cambio a logo de inicio

--- formulario.js
+++ formulario.js
@@ -1,3 +1,33 @@
+// TARJETAS DE PROYECTOS
+
+const gitsArray = ["proyects/memo.png"]
+
+const selectors = {
+  proyectosContainer: document.querySelector(".proyectos-container"),
+  tarjetasGrid: document.querySelector(".tarjetas-grid")
+}
+
+const dimension = selectors.tarjetasGrid.getAttribute("data-dimension")
+
+const generarGrid = () => {
+  const imagen = ( gitsArray, dimension * dimension / 2 )
+  const items = (imagen)
+  const tarjeta = items.reduce((acc, element) => {
+    return acc + `
+    <div class="tarjeta">
+      <div class="tarjeta-frente">
+      </div>
+      <div class="tarjeta-reverso">
+      ${element}
+      </div>
+    </div>
+    `
+  }, "")
+  selectors.tarjetasGrid.innerHTML = tarjeta
+}
+
+//   FORMULARIO
+
 const btn = document.getElementById('button');
 
 document.getElementById('formulario')
