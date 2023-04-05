d43910118880f27af90714c37e4951f64c810055
Araceli Fernandez <aracelifernandez.3006@live.com.ar>
2023-04-04 23:05:23 -0300

    Button-UP funcional con Javascript

--- formulario.js
+++ formulario.js
@@ -1,4 +1,31 @@
 
+//  SCROLL UP
+
+document.getElementById('button-up').addEventListener('click', scrollUp)
+
+function scrollUp() {
+  var currentScroll = document.documentElement.scrollTop;
+
+  if (currentScroll > 0) {
+    window.requestAnimationFrame(scrollUp)
+    window.scrollTo (0, currentScroll - (currentScroll / 20));
+  }
+}
+
+/// EL BUTTON-UP APARECE Y DESAPARECE CUANDO HACEMOS SCROLL
+
+buttonUp = document.getElementById('button-up');
+
+window.onscroll = function(){
+  var scroll = document.documentElement.scrollTop;
+  
+  if (scroll > 300) {
+    buttonUp.style.transform = 'scale(1)';
+  } else if (scroll <300) {
+    buttonUp.style.transform = 'scale(0)';
+  }
+}
+
 
 //   FORMULARIO
 
@@ -23,4 +50,5 @@ document.getElementById('formulario')
       alert(JSON.stringify(err));
     });
 });
+
 
